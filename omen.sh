04ba76089d17dd17377ed3d7f9450a9af0f2be35
IvoST <94892775+ivomed@users.noreply.github.com>
2023-01-02 10:54:29 +0100

    Update omen.sh

--- omen.sh
+++ omen.sh
@@ -54,6 +54,13 @@ sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key FBA220DFC880C036
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+echo "[chaotic-aur]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+
 #----------------------------------SERVICES-------------------------------
 
 systemctl enable NetworkManager
