9a7029257a92c3b60b5d09ca463f9f405a73eb0c
IvoST <94892775+ivomed@users.noreply.github.com>
2023-01-02 10:48:54 +0100

    Update dragon.sh

--- dragon.sh
+++ dragon.sh
@@ -54,6 +54,11 @@ sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key FBA220DFC880C036
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+
 #----------------------------------SERVICES-------------------------------
 
 systemctl enable NetworkManager
